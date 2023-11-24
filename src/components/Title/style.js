f707d2f616a214c36ef343cbcd5e7f4cd6867c02
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-11-24 08:54:35 -0300

    add styles in Title

--- src/components/Title/style.js
+++ src/components/Title/style.js
@@ -1,7 +1,15 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  Title: {
+  boxTitle: {
     alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  textTitle: {
+    color: "#FF0043",
+    fontWeight: "bold",
+    fontSize: 24,
+    textTransform: "uppercase",
   },
 });
