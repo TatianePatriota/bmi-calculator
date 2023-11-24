b51e8b922d78e770f89f024e9cb0ff8f18dcb25d
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-11-24 16:27:01 -0300

    add onShare function

--- src/components/ResultImc/ResultImc.js
+++ src/components/ResultImc/ResultImc.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import { styles } from "./style";
+import { Share } from "react-native";
 
 export default function ResultImc(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: "Meu IMC hoje é: " + props.imcResult,
+    });
+  };
   return (
     <View style={styles.resultImc}>
+      <View style={styles.boxSharebutton}>
+        {props.imcResult != null ? (
+          <TouchableOpacity style={styles.shared} onPress={onShare}>
+            <Text style={styles.sharedText}>Compartilhar</Text>
+          </TouchableOpacity>
+        ) : (
+          <View />
+        )}
+      </View>
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.numberImc}>{props.imcResult}</Text>
     </View>
