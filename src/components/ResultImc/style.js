dc7c26c901409d1ecba6c0de92896505a59d9ba4
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-11-24 09:49:58 -0300

    created resultImc styles

---
+++ src/components/ResultImc/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  resultImc: {
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 30,
+    borderRadius: 20,
+    alignItems: "center",
+    width: "100%",
+  },
+  numberImc: {
+    color: "#ff0043",
+    fontSize: 45,
+    fontWeight: "bold",
+  },
+  information: {
+    fontSize: 18,
+    color: "#ff0043",
+    fontWeight: "bold",
+  },
+});
